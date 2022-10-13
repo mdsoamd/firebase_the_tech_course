dba7d44a2b8d0c2a5c82498b8605e7fc0f4c3fd2
Md Somad <mdsomad10@gmail.com>
2022-10-13 21:02:14 +0530

    Stream Builder Complete

--- lib/ui/posts/post.dart
+++ lib/ui/posts/post.dart
@@ -7,9 +7,6 @@ import 'package:firebase_the_tech_course/utils/utils.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-
-
 class PostScreen extends StatefulWidget {
   const PostScreen({Key? key}) : super(key: key);
 
@@ -18,64 +15,101 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
-   
- final _auth = FirebaseAuth.instance;
-  
- final ref = FirebaseDatabase.instance.ref("Post");
+  final _auth = FirebaseAuth.instance;
+
+  final ref = FirebaseDatabase.instance.ref("Post");
 
 
- void logout(){      //  <-- this logout Method code 
 
-   _auth.signOut().then((value){
-    Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginScreen()));})
-    .onError((error, stackTrace){
-       Utils().tostMessage(error.toString());
-   });
- }
-  
+
+  void logout() {                      //  <-- this logout Method code
+    _auth.signOut().then((value) {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => LoginScreen()));
+    }).onError((error, stackTrace) {
+      Utils().tostMessage(error.toString());
+    });
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      appBar:  AppBar(
+      appBar: AppBar(
         title: Text("Post"),
         actions: [
-          IconButton(onPressed: (){
-           logout();  // <-- This Call LogOut Method
-          }, 
-          icon: Icon(Icons.login_outlined)),
-          SizedBox(width: 10,)
+          IconButton(
+              onPressed: () {
+                logout(); // <-- This Call LogOut Method
+              },
+              icon: Icon(Icons.login_outlined)),
+          SizedBox(
+            width: 10,
+          )
         ],
       ),
-
-
-      body:  Column(
+      body: Column(
         children: [
 
+
           Expanded(
-            child: FirebaseAnimatedList(          // <-- Yah Data Show kerne ka Pahla Tarika  
-            query: ref, 
-            defaultChild: Center(child: Text("Loading")),
+              child: StreamBuilder(                 // <-- Yah Data Show kerne ka durra Tarika   (Stap 2  Data Display Method)
+            stream: ref.onValue,
+            builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
 
-            itemBuilder: (context, snapshot, animation, index) { 
-                         
-              return ListTile(                                          
-              title: Text(snapshot.child("title").value.toString()),
-              subtitle: Text(snapshot.child("id").value.toString()),
-              );
-            }
-          
-            ),
+              if (!snapshot.hasData) {
+                
+                return CircularProgressIndicator();
+
+              } else {
+
+                Map<dynamic, dynamic> map = snapshot.data!.snapshot.value as dynamic;
+                List<dynamic> list = [];   // <-- This All Data Store List
+                list.clear();
+                list = map.values.toList();
+
+                return ListView.builder(
+                    itemCount: snapshot.data!.snapshot.children.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(list[index]['title']),
+                        subtitle: Text(list[index]['id']),
+                      );
+                    });
+              }
+            },
+          )),
+
+
+
+          Expanded(
+            child: FirebaseAnimatedList(           // <-- Yah Data Show kerne ka Pahla Tarika    (Stap 1 Data Display Method)
+                query: ref,
+                defaultChild: Center(child: Text("Loading")),
+                itemBuilder: (context, snapshot, animation, index) {
+
+                  return ListTile(
+                    title: Text(snapshot.child("title").value.toString()),
+                    subtitle: Text(snapshot.child("id").value.toString()),
+                  );
+                }),
           )
-          
         ],
-      ),
 
-        
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        Navigator.push(context, MaterialPageRoute(builder: (context)=>AddPostScreen()));
-      },
-      child: Icon(Icons.add,),
+
+      ), // Column
+
+
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => AddPostScreen()));
+        },
+        child: Icon(
+          Icons.add,
+        ),
       ),
     );
   }
